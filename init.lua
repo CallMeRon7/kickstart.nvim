fa37e749bbd2ce75ef2aad85b50c9c475c11ed3f
Cameron Haire <cdhaire08@gmail.com>
2025-08-24 23:35:43 -0400

    Add texlab line to init.lua

--- init.lua
+++ init.lua
@@ -679,6 +679,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        texlab = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
